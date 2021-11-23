89f920aaa0bca41190c602d338ae3190f5806d7f
francocortina <franco.cug@gmail.com>
2021-11-23 18:39:36 -0300

    agreggo un componente

---
+++ src/Componentes/ValorCripto.jsx
@@ -0,0 +1,20 @@
+
+import  React, {Component}  from 'react'
+import  './styles/Card.css'
+import axios from 'axios'
+
+class ValorCripto extends Component{
+
+    componentDidMount(){
+        axios.get('http://127.0.0.1:5000/api/BTC/')
+        .then(result=> { console.log(result)})
+        .catch(console.log)
+    } 
+    render(){
+        return(
+        <h1>no anda loco</h1>)
+    }
+   
+
+}
+export default ValorCripto;
